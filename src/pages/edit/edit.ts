c7d10cfafe32339cb97666e6a3e84157f85b2baa
Elvynia <elvynia@gmail.com>
2018-03-28 16:50:18 +0200

    Implémentation des pages list et edit + ajouts des assets.

--- src/pages/edit/edit.ts
+++ src/pages/edit/edit.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Article } from 'betterblog';
+import { ListPage } from '../list/list';
 
 @IonicPage()
 @Component({
@@ -7,12 +9,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 	templateUrl: 'edit.html',
 })
 export class EditPage {
+	editArticle: Article;
 
 	constructor(public navCtrl: NavController, public navParams: NavParams) {
+		this.editArticle = this.navParams.data.edit || new Article();
 	}
 
-	ionViewDidLoad() {
-		console.log('ionViewDidLoad EditPage');
+	create(article: Article) {
+		this.navCtrl.getPrevious().data.create = article;
+		this.handleNav();
+	}
+
+	update(article: Article) {
+		this.navCtrl.getPrevious().data.update = article;
+		this.navCtrl.getPrevious().data.edit = undefined;
+		this.handleNav();
+	}
+
+	private handleNav() {
+		if (this.navCtrl.length() > 1) {
+			this.navCtrl.pop();
+		} else {
+			this.navCtrl.setRoot(ListPage);
+		}
 	}
 
 }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ArticleService, Article } from 'betterblog';
+import { EditPage } from '../edit/edit';
 
 @IonicPage()
 @Component({
@@ -7,12 +9,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 	templateUrl: 'list.html',
 })
 export class ListPage {
+	list: Array<Article>;
 
-	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	constructor(public navCtrl: NavController, public navParams: NavParams,
+		private articleService: ArticleService) {
+		this.list = new Array();
 	}
 
 	ionViewDidLoad() {
-		console.log('ionViewDidLoad ListPage');
+		this.articleService.list().subscribe((list) => {
+			this.list = list;
+		});
 	}
 
+	ionViewDidEnter() {
+		let data = this.navParams.data;
+		if (data.create) {
+			this.list.push(data.create);
+			data.create = undefined;
+		}
+		if (data.update) {
+			let index = this.list.findIndex((a) => a.id === data.update.id);
+			this.list.splice(index, 1, data.update);
+			data.update = undefined;
+		}
+	}
+
+	modify(id: number, index: number) {
+		this.navCtrl.push(EditPage, {
+			edit: this.list[index]
+		});
+	}
+
+	delete(id: number, index: number) {
+		this.list.splice(index, 1);
+	}
 }
