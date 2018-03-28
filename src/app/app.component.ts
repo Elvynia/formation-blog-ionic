175cc4dff495f62e4b4f9e04b3ed63a405d44203
Elvynia <elvynia@gmail.com>
2018-03-28 15:39:10 +0200

    Préparation de la navigation : création des pages, placement de la navbar dans la templete du component App pour qu'elle soit présente sur toutes les pages. Les pages ne contiennent donc que le <ion-content> mais pas de header. Gestion de la navigation des pages avec l'événement onNavigate de la navbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { NAV_LIST, NAV_CREATE, NAV_CONTACT, NAV_HOME } from 'betterblog';
+
 import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { EditPage } from '../pages/edit/edit';
+import { ContactPage } from '../pages/contact/contact';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -17,6 +23,20 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  navigate(path: string) {
+    if (path === NAV_HOME) {
+      this.rootPage = HomePage;
+    } else if (path === NAV_LIST) {
+      this.rootPage = ListPage;
+    } else if (path === NAV_CREATE) {
+      this.rootPage = EditPage;
+    } else if (path === NAV_CONTACT) {
+      this.rootPage = ContactPage;
+    } else {
+      console.error('Navigation path %s not managed by ion-blog...', path);
+    }
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { BlogLibModule } from 'betterblog';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { EditPage } from '../pages/edit/edit';
+import { ContactPage } from '../pages/contact/contact';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ListPage,
+    EditPage,
+    ContactPage
   ],
   imports: [
     BrowserModule,
@@ -21,7 +27,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ListPage,
+    EditPage,
+    ContactPage
   ],
   providers: [
     StatusBar,
