38453d90a9ef6026f256eb0315fd9c9c29b8e971
elvynia <elvynia@gmail.com>
2018-03-30 11:19:07 +0200

    Ajout d'un bouon pour déclencher le chargement du fichier JSON. Affichage d'une alerte Ionic avec AlertController lorsque le chargement du JSON est en erreur. Gestion d'un bouton retry pour relancer le chargement depuis la popup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ArticleService } from 'betterblog';
@@ -18,13 +18,32 @@ export class MyApp {
 	rootPage: any = HomePage;
 
 	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
-		private articleService: ArticleService) {
+		private alertCtrl: AlertController, private articleService: ArticleService) {
 		platform.ready().then(() => {
-			this.articleService.initialize();
 			// Okay, so the platform is ready and our plugins are available.
 			// Here you can do any higher level native things you might need.
 			statusBar.styleDefault();
 			splashScreen.hide();
+		});
+	}
+
+	loadData() {
+		this.articleService.initialize().subscribe({
+			error: (response) => {
+				this.alertCtrl.create({
+					title: 'Erreur de chargement',
+					subTitle: 'Impossible de charger le fichier de données JSON',
+					buttons: [{
+						text: 'OK',
+						role: 'cancel'
+					}, {
+						text: 'Réessayer',
+						handler: () => {
+							setTimeout(() => this.loadData());
+						}
+					}]
+				}).present();
+			}
 		});
 	}
 
